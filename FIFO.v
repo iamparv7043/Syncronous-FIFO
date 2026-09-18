493eb75fcd2b3b2df7736f5cc05b3577d580f977
Parv <155349219+iamparv7043@users.noreply.github.com>
2026-09-18 16:15:27 +0530

    Refactor FIFO module with simplified structure

--- FIFO.v
+++ FIFO.v
@@ -1,55 +1,49 @@
-module sync_fifo #(
-    parameter DATA_WIDTH = 8,
-    parameter DEPTH      = 16
-)(
-    input clk,rst,rd_en,wr_en,
-    input      [DATA_WIDTH-1:0] din,
-    output reg [DATA_WIDTH-1:0] dout,
-    output full,empty
+module fifo(
+    input clk, rst, rd, wr,
+    input [7:0] din,
+    output reg [7:0] dout,
+    output full, empty
 );
 
-    localparam ADDR_WIDTH = $clog2(DEPTH);
+reg [7:0] mem [31:0];
+reg [5:0] rdptr, wrptr;
 
-    // Extra bit is used for full/empty detection
-    reg [ADDR_WIDTH:0] rd_ptr;
-    reg [ADDR_WIDTH:0] wr_ptr;
-
-    reg [DATA_WIDTH-1:0] fifo [0:DEPTH-1];
-
-    // Simultaneous read conditions
-    wire simult_rw_when_full = full && rd_en && wr_en;
-    // simultabeous write condition
-    wire simult_rw_when_empty = empty && rd_en && wr_en;
-  
-    // Allow read/write
-    wire wr_allowed = (!full) || simult_rw_when_full;
-    wire rd_allowed = (!empty) || simult_rw_when_empty;
-   
-    // FIFO operation
-    always @(posedge clk) begin
-     if (rst) begin
-            dout   <= {DATA_WIDTH{1'b0}};
-            rd_ptr <= {(ADDR_WIDTH+1){1'b0}};
-            wr_ptr <= {(ADDR_WIDTH+1){1'b0}};
-     end
-
-      else begin
-        if (rd_en && rd_allowed ) begin  //rd operation
-                rd_ptr <= rd_ptr + 1'b1;
-                if (simult_rw_when_empty || simult_rw_when_full)
-                    dout <= din;
-                else
-                    dout <= fifo[rd_ptr[ADDR_WIDTH-1:0]];
+always @(posedge clk) begin
+    if (rst) begin
+        dout  <= 0;
+        rdptr <= 0;
+        wrptr <= 0;
+    end
+    else begin
+        case ({wr, rd})
+            2'b01: begin // read
+                if (!empty) begin
+                    dout  <= mem[rdptr[4:0]];
+                    rdptr <= rdptr + 1;
+                end
             end
-          
-          if (wr_en && wr_allowed) begin   //write opration
-                fifo[wr_ptr[ADDR_WIDTH-1:0]] <= din;
-                wr_ptr <= wr_ptr + 1'b1;
-          end
-      end
-  end
-  
-  assign empty = (rd_ptr == wr_ptr);  // empty condition
-  assign full  = (rd_ptr == {~wr_ptr[ADDR_WIDTH],wr_ptr[ADDR_WIDTH-1:0]});  // full condition 
+            2'b10: begin // write
+                if (!full) begin
+                    mem[wrptr[4:0]] <= din;
+                    wrptr <= wrptr + 1;
+                end
+            end
+            2'b11: begin // simultaneous read/write
+                if (empty) begin
+                    dout <= din; 
+                end
+                else begin
+                    mem[wrptr[4:0]] <= din;
+                    wrptr <= wrptr + 1;
+                    dout  <= mem[rdptr[4:0]];
+                    rdptr <= rdptr + 1;
+                end
+            end
+        endcase
+    end
+end
+
+assign empty = (rdptr == wrptr);
+assign full  = (rdptr == {~wrptr[5], wrptr[4:0]});
 
 endmodule
